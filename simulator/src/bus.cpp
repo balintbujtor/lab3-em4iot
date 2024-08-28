5894c0ce256c4a35b9d4d7d665d6186bd7f42503
Bálint Bujtor <balint.bujtor@gmail.com>
2024-08-28 18:48:03 +0200

    upgraded the bus policy

--- simulator/src/bus.cpp
+++ simulator/src/bus.cpp
@@ -7,15 +7,32 @@ void bus::initialize() {}
 
 void bus::processing()
 {
+    const double supercap_limit = 225; // mA for 40s supply
+    const double supercap_request_threshold = 30; //above this we use the supercap
+
+    double tot_requested, tot_consumed, tot_batt_requested, tot_sc_requested;
+
     // Compute total current consumption
-    double tot_consumed = i_mcu.read()
+    tot_consumed = i_mcu.read()
                           + i_air_quality_sensor.read()
                           ;
 
-    double tot_requested = tot_consumed;
-    
-    double tot_batt_requested = 0.9 * tot_requested;
-    double tot_sc_requested = tot_requested - tot_batt_requested;
+    tot_requested = tot_consumed;
+
+    if(tot_requested > supercap_request_threshold && tot_requested < supercap_limit){
+        tot_sc_requested = tot_requested;
+        tot_batt_requested = 0;
+    }
+    else if(tot_requested > supercap_limit) 
+    {
+        tot_sc_requested = supercap_limit;
+        tot_batt_requested = tot_requested - supercap_limit;
+    }
+    else {
+        //below supercap threshold, everything by battery
+        tot_sc_requested = 0;
+        tot_batt_requested = tot_requested;
+    }
 
     i_tot_batt.write(tot_batt_requested); // tot_requested >= 0 ? pow_from_battery : pow_to_battery
     i_tot_sc.write(tot_sc_requested); 
